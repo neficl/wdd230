077f5d0e7a0a0c7cc68c61a1b7cde806f4aab105
Néfi Leite <nefi.leite@gmail.com>
2023-01-19 02:47:05 -0300

    Week 03

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,27 +1,16 @@
 const modeButton = document.querySelector("#mode");
-const darkness = document.querySelector("section");
-const body = document.getElementsByTagName("body")[0];
-const darkness1 = document.getElementById("darkmode1");
-const darkness2 = document.getElementById("darkmode2");
+const main = document.querySelector("main");
+
+modeButton.textContent = "☑️ Dark Mode"
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("☑️")) {
-        body.style.background = "#fff"
-		darkness.style.background = "#fff";
-		darkness1.style.background = "#fff";
-		darkness2.style.background = "#fff";
-		darkness.style.color = "#000";
-		darkness1.style.color = "#000";
-		darkness2.style.color = "#000";
-		modeButton.textContent = "❎ Dark Mode Off";
+		main.style.background = "#000";
+		main.style.color = "#000";
+		modeButton.textContent = "❎ Dark Mode ON";
 	} else {
-        body.style.background = "#000"
-		darkness.style.background = "#000";
-		darkness1.style.background = "#000";
-		darkness2.style.background = "#000";
-		darkness.style.color = "#fff";
-		darkness1.style.color = "#fff";
-		darkness2.style.color = "#fff";
-		modeButton.textContent = "☑️ Dark Mode On";
+		main.style.background = "#4682b4";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️ Dark Mode";
 	}
 });
---
+++ week03/scripts/bom.js
@@ -0,0 +1,51 @@
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+const list = document.querySelector('ul');
+
+// add this array declaration and assignment statement
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+  displayList(chapter);
+});
+
+button.addEventListener('click', () => {
+  if (input.value != '') {
+    displayList(input.value);
+    chaptersArray.push(input.value);
+    setChapterList();
+    input.value = '';
+    input.focus();
+  }
+});
+
+function displayList(item) {
+  let li = document.createElement('li');
+  let deletebutton = document.createElement('button');
+  li.textContent = item; // note the use of the item parameter and then delete this comment
+  deletebutton.textContent = '❌';
+  deletebutton.classList.add('delete'); // this references the CSS rule .delete{width:fit-content;} to size the delete button
+  li.append(deletebutton);
+  list.append(li);
+  deletebutton.addEventListener('click', function () {
+    list.removeChild(li);
+    deleteChapter(li.textContent); // note this new function that is needed to remove the chapter from the array and localStorage.
+    input.focus();
+  });
+}
+
+function setChapterList() {
+  localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+  return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+chapter = chapter.slice(0, chapter.length - 1); // this slices off the last character
+chaptersArray = chaptersArray.filter((item) => item !== chapter);
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chaptersArray.filter(item => item !== chapter);
+  setChapterList();
+}
